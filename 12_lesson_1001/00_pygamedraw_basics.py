f927403294ef490fd22b680296e5bc08374b3a99
demidraksin <greatraksin@icloud.com>
2022-01-31 08:53:45 +0300

    Fixing starter template

--- 12_lesson_1001/00_pygamedraw_basics.py
+++ 12_lesson_1001/00_pygamedraw_basics.py
@@ -1,12 +1,13 @@
 import pygame as pg
+from pygame.draw import rect, circle, polygon
 
+W = 640
+H = 640
 # создаю объект окна программы
-s = pg.display.set_mode((640, 480))  # 640pх - ширина и 480px - высота
+screen = pg.display.set_mode((W, H))  # 640pх - ширина и 480px - высота
 clock = pg.time.Clock()  # добавление задержки в главный цикл
 
-# рисуем тут
 
-pg.display.update()
 finished = False  # флаг, который отвечает за работу программы
 while not finished:  # пока игра не окончена
     clock.tick(30)  # частота обновления 30 кадров в секунду
@@ -14,4 +15,7 @@ while not finished:  # пока игра не окончена
     for event in pg.event.get():  # для каждого события в списке событий
         if event.type == pg.QUIT:  # если нажали на крестик
             finished = True
+
+    # рисуем тут
+    pg.display.update()
 
