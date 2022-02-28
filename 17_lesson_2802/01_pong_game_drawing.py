2fab74d4eace22c150caec10b0053059390855cb
demidraksin <greatraksin@icloud.com>
2022-02-28 09:25:53 +0300

    Finishing drawing and game visuals

--- 17_lesson_2802/01_pong_game_drawing.py
+++ 17_lesson_2802/01_pong_game_drawing.py
@@ -1,5 +1,6 @@
 import pygame as pg
-from pygame.draw import rect, circle, polygon
+from pygame.draw import rect, ellipse, aaline
+from random import randint
 
 
 def show_message(txt, color, x, y, size):
@@ -18,10 +19,20 @@ def show_message(txt, color, x, y, size):
     screen.blit(msg, [x, y])
 
 
-W = 640
-H = 640
+W = 1280
+H = 960
 screen = pg.display.set_mode((W, H))
+pg.display.set_caption('Pong game v1🏓')
 clock = pg.time.Clock()
+
+# Colors
+l_gray = (200, 200, 200)
+bg_color = (107, 107, 107)
+
+# Game objects
+ball = pg.Rect(W // 2 - 15, H // 2 - 15, 30, 30)
+platform = pg.Rect(W - 20, H // 2, 10, 150)
+opponent = pg.Rect(10, H // 2, 10, 150)
 
 finished = False
 while not finished:
@@ -29,5 +40,12 @@ while not finished:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             finished = True
+
+    # Visuals
+    screen.fill(bg_color)
+    rect(screen, l_gray, platform)
+    rect(screen, l_gray, opponent)
+    ellipse(screen, l_gray, ball)
+    aaline(screen, l_gray, [W // 2, 0], [W // 2, H])
 
     pg.display.update()
